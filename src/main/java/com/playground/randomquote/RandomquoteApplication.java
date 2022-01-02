cbd0ca0b47521f43e5ab235b1a805af80ad5840e
Dusko <duskox@gmail.com>
2022-01-02 02:39:13 +0100

    Initial commit

---
+++ src/main/java/com/playground/randomquote/RandomquoteApplication.java
@@ -0,0 +1,13 @@
+package com.playground.randomquote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomquoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomquoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playground/randomquote/RandomquoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.randomquote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomquoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
